5efc7dcf693fe27ba611070c47c88a676883b431
niyato2001 <niyato2001@gmail.com>
2021-12-09 15:48:13 +0900

    HEADERの作成

--- src/pages/header.js
+++ src/pages/header.js
@@ -1,6 +1,27 @@
-import Head from <next Head>
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
 
-<Head>
+function indexPage(){
+    return(
+        <div>
+            <div id="container" absolute left-0 top-0 h-16 w-16>
+                <Image src="/logo.png" alt="Head log" width={200} height={100}/>
+            </div>
+                <Link href="#about">
+                    <a>ABOUT</a>
+                </Link>
+                <Link href="#skills">
+                    <a>SKILLS</a>
+                </Link>
+                <Link href="#values">
+                    <a>VALUES</a>
+                </Link>
+                <Link href="#future">
+                    <a>FUTURE</a>
+                </Link>   
+        </div>
+    )
+};
 
-    
-</Head>
+export default indexPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import Head from 'next/head'
+import Image from 'next/image'
+import indexPage from './header'
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <indexPage/>
+          <p>ABC</p>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
@@ -67,15 +70,7 @@ export default function Home() {
       </main>
 
       <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
+
       </footer>
     </div>
   )
