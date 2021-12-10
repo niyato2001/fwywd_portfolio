9b57488b98877f34fce279ea34220c208508e1f8
niyato2001 <niyato2001@gmail.com>
2021-12-10 21:27:11 +0900

    footer見た目完成

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,32 +1,33 @@
 import Image from 'next/image';
 
 
-const Footer = () => {
-    return (
-        <div className="bg-black text-center w-full h-100">
-            <div className="text-white">
-                    <div>
-                        <p className="text-xs float-left">CREATED BY</p>
-                        <p className="float-none">田原 純平</p>
+const Footer = () => (
+        <div className="bg-black text-center w-full h-auto text-white">
+            <div>
+                    <div className="items-center ml-auto mr-auto my-10 tracking-wide">
+                        <span className="text-xs mx-1">CREATED BY</span>
+                        <span className="mx-1 font-bold">田原 純平</span>
                     </div>
 
             </div>
-            <div className="footerLogo">
-                <Image src="/introduction/logo_white.png" width={100} height={100} />
+            <div className="my-3">
+                <Image src="/introduction/logo_white.png" width={120} height={144} />
             </div>
-            <div className="snsLogo">
-                <div className="float-left">
-                    <Image src="/introduction/twitter.png" width={30} height={30} />
-                </div>
-                <div className="float-none">
-                    <Image src="/introduction/facebook.png" width={30} height={30} />
-                </div>
-                <div className="text-white">
-                    <p>© 2021 KIKAGAKU</p>
+            <div>
+                <div className="items-center ml-auto mr-auto my-8">
+                     <div className="inline mx-2">
+                          <Image src="/introduction/twitter.png" width={24} height={20} />
+                     </div>
+                     <div className="inline mx-2">
+                          <Image src="/introduction/facebook.png" width={20} height={20} />
+                     </div>
                 </div>
             </div>
+            <div>
+                    <p className="tracking-wider mb-16">© 2021 KIKAGAKU</p>
+            </div>
+            
         </div>
-    )
-};
+    );
 
 export default Footer;
