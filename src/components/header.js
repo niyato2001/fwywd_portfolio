bdac9f23e4529e9e6b4d4c323b78cce3d612c0e7
niyato2001 <niyato2001@gmail.com>
2021-12-10 17:03:49 +0900

    flex導入

--- src/components/header.js
+++ src/components/header.js
@@ -9,24 +9,24 @@ const Header = () => (
                 <Image src="/logo.png" alt="Head log" width={192} height={96} />
             </div>
 
-            <div className="float-right">
-                <ul  className="text-green-font h-24 flex items-center">
-                <li className="mx-2">
+            <div className="float-right mr-2">
+                <ul  className="text-green-font h-24 flex items-center font-bold tracking-wider text-lg">
+                <li className="mx-5">
                 <Link href="#about">
                     <a className="">ABOUT</a>
                 </Link>
                 </li>
-                <li className="mx-2">
+                <li className="mx-5">
                 <Link href="#skills">
                     <a className="">SKILLS</a>
                 </Link>
                 </li>
-                <li className="mx-2">
+                <li className="mx-5">
                 <Link href="#values">
                     <a className="">VALUES</a>
                 </Link>
                 </li>
-                <li className="mx-2">
+                <li className="mx-5">
                 <Link href="#future">
                     <a className="">FUTURE</a>
                 </Link>
@@ -34,8 +34,8 @@ const Header = () => (
                 </ul>
             </div> 
         </div>
-        <div className="bg-green-bg w-full h-auto overflow-hidden">
-                <div className="float-left px-24 py-60 align-middle ">
+        <div className="bg-green-bg w-full h-auto   flex items-center">
+                <div className="ml-10 justify-start">
                     <h1 className="text-white text-4xl">
                         遠隔型皮膚外用薬案内システム（仮）
                      </h1>
@@ -43,7 +43,7 @@ const Header = () => (
                             セルフメディケーションを加速する
                         </h2>
                 </div>
-                <div className="float-right px-12 py-12">
+                <div className="ml-auto mr-12 py-12">
                     <Image src="/hero.png" alt="header" width={560} height={480}/>
                 </div>
             
