e91c1e40bbe5481a0e1138f5df20fe8f9f4acdbb
niyato2001 <niyato2001@gmail.com>
2021-12-10 14:12:30 +0900

    色拡張,header追加

--- src/components/header.js
+++ src/components/header.js
@@ -3,36 +3,52 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 const Header = () => (
-        <div>
-            <div>
-                <Image src="/logo.png" alt="Head log" width={200} height={100} float-left/>
+    <div>
+        <div className="overflow-hidden w-full h-24">
+            <div className="float-left">
+                <Image src="/logo.png" alt="Head log" width={192} height={96} />
             </div>
 
-            <div>
-                <ul  style-none>
-                <li float-right>
+            <div className="">
+                <ul  className="float-right text-green-font px-4 py-6 flex h-24">
+                <li className="float-left table-cell align-middle">
                 <Link href="#about">
-                    <a>ABOUT</a>
+                    <a className="">ABOUT</a>
                 </Link>
                 </li>
-                <li>
+                <li className="float-left table-cell align-middle">
                 <Link href="#skills">
-                    <a>SKILLS</a>
+                    <a className="">SKILLS</a>
                 </Link>
                 </li>
-                <li>
+                <li className="float-left table-cell align-middle">
                 <Link href="#values">
-                    <a>VALUES</a>
+                    <a className="">VALUES</a>
                 </Link>
                 </li>
-                <li>
+                <li className="float-left table-cell align-middle">
                 <Link href="#future">
-                    <a>FUTURE</a>
+                    <a className="">FUTURE</a>
                 </Link>
                 </li>
                 </ul>
-            </div>       
+            </div> 
         </div>
+        <div className="bg-green-bg w-full h-auto overflow-hidden">
+                <div className="float-left px-24 py-60 align-middle ">
+                    <h1 className="text-white text-4xl">
+                        遠隔型皮膚外用薬案内システム（仮）
+                     </h1>
+                        <h2 className="text-green-prolight text-2xl py-2">
+                            セルフメディケーションを加速する
+                        </h2>
+                </div>
+                <div className="float-right px-12 py-12">
+                    <Image src="/hero.png" alt="header" width={560} height={480}/>
+                </div>
+            
+        </div>      
+    </div>
     );
 
 export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,21 @@ module.exports = {
   purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        black:{
+          font:'#384359',
+          bg:'#262c3a',
+        },
+        green:{
+          font:'#008c8d',
+          bg:'#6bc2c3',
+          prolight:'#c5eaea',
+          prodark:'#2bb9ba',
+          line:'#c5eaea',
+        }
+      },
+    },
   },
   variants: {
     extend: {},
