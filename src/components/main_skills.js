53754e8f155bdfeab102697a7d2d62027db15897
niyato2001 <niyato2001@gmail.com>
2021-12-17 13:04:47 +0900

    skills progress bar完成

--- src/components/main_skills.js
+++ src/components/main_skills.js
@@ -2,10 +2,56 @@ import Image from 'next/image';
 
 const Skills = () => {
   return (
-    <div id='skills' className='bg-skills bg-cover -mt-16'>
-      <div className=''></div>
+    <div id='skills' className='bg-skills -mt-12'>
+      <div className='w-2/3 h-auto flex mx-auto py-32'>
+        <div className='items-center w-1/2 mt-16 mx-7'>
+          <Image src='/introduction/skill.png' alt='skill' width={535} height={372} />
+        </div>
+        <div className='items-center w-1/2'>
+          <h1 className='inline w-1/2 text-black-font text-3xl font-bold mx-1 align-middle tracking-widest'>
+            スキル
+          </h1>
+          <p className='inline w-1/2 text-green-font font-semibold mx-1 align-middle tracking-wider'>
+            SKILLS
+          </p>
+          <p className='text-black-font tracking-widest text-base leading-loose mt-5'>
+            テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+            テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+          </p>
+          <div className='bg-white p-7'>
+            <p className='text-green-font font-semibold tracking-wider'>SKILLSKILL</p>
+            <div className='flex items-center'>
+              <div className='w-full h-3.5 bg-green-prolight rounded'>
+                <div className='h-3.5 bg-green-prodark w-2/5 rounded-l'></div>
+              </div>
+              <span className=' text-green-font tracking-wider mx-4'>40%</span>
+            </div>
+            <p className='text-green-font font-semibold tracking-wider'>SKILLSKILL</p>
+            <div className='flex items-center'>
+              <div className='w-full h-3.5 bg-green-prolight rounded'>
+                <div className='h-3.5 bg-green-prodark w-2/5 rounded-l'></div>
+              </div>
+              <span className=' text-green-font tracking-wider mx-4'>40%</span>
+            </div>
+            <p className='text-green-font font-semibold tracking-wider'>SKILLSKILL</p>
+            <div className='flex items-center'>
+              <div className='w-full h-3.5 bg-green-prolight rounded'>
+                <div className='h-3.5 bg-green-prodark w-2/5 rounded-l'></div>
+              </div>
+              <span className=' text-green-font tracking-wider mx-4'>40%</span>
+            </div>
+            <p className='text-green-font font-semibold tracking-wider'>SKILLSKILL</p>
+            <div className='flex items-center'>
+              <div className='w-full h-3.5 bg-green-prolight rounded'>
+                <div className='h-3.5 bg-green-prodark w-2/5 rounded-l'></div>
+              </div>
+              <span className=' text-green-font tracking-wider mx-4'>40%</span>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className='h-96'></div>
-      <div className='h-96'></div>
+      <div className='h-48'></div>
     </div>
   );
 };
