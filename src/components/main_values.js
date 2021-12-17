477d1c58e727a9ca360c4595ab38596a55d0e14c
niyato2001 <niyato2001@gmail.com>
2021-12-18 00:01:25 +0900

    テキスト下padding OK

--- src/components/main_values.js
+++ src/components/main_values.js
@@ -2,8 +2,8 @@ import Image from 'next/image';
 
 const Values = () => {
   return (
-    <div>
-      <div className='w-2/3 mx-auto bg-white -mb-60'>
+    <div className='-mb-60'>
+      <div className='w-2/3 mx-auto bg-white'>
         <div className='text-center py-12 mb-10'>
           <h2
             id='values'
@@ -13,14 +13,14 @@ const Values = () => {
           </h2>
           <p className='inline mx-4 text-lg align-middle text-bold text-green-font'>VALUES</p>
         </div>
-        <div className='flex items-end text-center bg-white'>
+        <div className='flex text-center bg-white'>
           <div className='w-1/3 h-auto px-8 border-r-2 border-green-line bg-white'>
             <div>
               <Image src='/introduction/value1.png' alt='value1' width={200} height={200} />
             </div>
             <p className='py-5 text-extrabold tracking-wider text-lg'>価値観01</p>
             <div className='text-black-font leading-loose items-center text-sm tracking-widest text-left'>
-              <p>
+              <p className='py-8'>
                 テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
                 テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
               </p>
@@ -32,7 +32,7 @@ const Values = () => {
             </div>
             <p className='py-5 text-extrabold tracking-wider text-lg'>価値観02</p>
             <div className='text-black-font leading-loose items-center text-sm tracking-widest text-left'>
-              <p>
+              <p className='py-8'>
                 テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
                 テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
               </p>
@@ -44,7 +44,7 @@ const Values = () => {
             </div>
             <p className='py-5 text-extrabold tracking-wider text-lg'>価値観03</p>
             <div className='text-black-font leading-loose items-center text-sm tracking-widest text-left'>
-              <p>
+              <p className='py-8'>
                 テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
                 テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
               </p>
