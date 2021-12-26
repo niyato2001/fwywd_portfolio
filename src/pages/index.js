e98df30a264c55af90eeb6a3b5b7af3711544543
niyato2001 <niyato2001@gmail.com>
2021-12-26 17:00:50 +0900

    headerレスポンシブ対応見た目OK

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,7 @@ export default function Home() {
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       </Head>
       <Header />
-
       <Main />
-
       <Footer />
     </div>
   );
