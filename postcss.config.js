c81ea491d8fe65e15ce5af786b36ae9bb46ff2ca
niyato2001 <niyato2001@gmail.com>
2021-12-31 18:21:57 +0900

    saas削除

--- postcss.config.js
+++ postcss.config.js
@@ -2,14 +2,7 @@
 // https://tailwindcss.com/docs/using-with-preprocessors
 module.exports = {
   plugins: {
-    tailwindcss: { config: './tailwind.config.js' },
+    tailwindcss: {},
     autoprefixer: {},
   },
 };
-
-const tailwindcss = require('tailwindcss');
-
-mix.sass('resources/sass/app.scss', 'public/css').options({
-  processCssUrls: false,
-  postCss: [tailwindcss('./path/to/your/tailwind.config.js')],
-});
