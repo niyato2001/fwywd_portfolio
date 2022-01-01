779175823a2f0cbca1ac577be35281fefb7ee22e
niyato2001 <niyato2001@gmail.com>
2022-01-01 18:45:12 +0900

    hoverのduration延長、headlessuiをとりあえずinstall

--- src/components/list.js
+++ src/components/list.js
@@ -8,7 +8,7 @@ class List extends React.Component {
   }
   render() {
     return (
-      <div className='table-cell px-2 h-10 hover:bg-green-prolight laptop_l:px-5 laptop_l:h-24'>
+      <div className='table-cell px-2 h-10 hover:bg-green-prolight laptop_l:px-5 laptop_l:h-24 duration-1000'>
         <Link href={this.props.link}>
           <a className='block h-10 py-2 laptop_l:h-24 laptop_l:py-8'>{this.props.name}</a>
         </Link>
