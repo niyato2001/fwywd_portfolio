700483fd7731a6cb6fa500398ba04183e0f16e3e
niyato2001 <niyato2001@gmail.com>
2022-01-01 16:05:53 +0900

    app.jsの;を削除

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }) {
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       </Head>
       <Layout>
-        <Component {...pageProps} />;
+        <Component {...pageProps} />
       </Layout>
     </>
   );
