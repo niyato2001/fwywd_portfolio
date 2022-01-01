ef5e31e87a47ae2d0ec90d5283c5e626f330c0d4
niyato2001 <niyato2001@gmail.com>
2022-01-01 10:58:12 +0900

    headerからタイトルを分離

--- src/components/main.js
+++ src/components/main.js
@@ -2,10 +2,12 @@ import Image from 'next/image';
 import About from './main_about';
 import Future from './main_future';
 import Skills from './main_skills';
+import Title from './main_title';
 import Values from './main_values';
 
 const Main = () => (
   <div className='w-full'>
+    <Title />
     <About />
     <Skills />
     <Values />
