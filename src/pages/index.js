49177975fe99e8e1713cade600bdab6c25339d72
niyato2001 <niyato2001@gmail.com>
2022-01-01 14:51:22 +0900

    index.jsのみtwitter OGP設定

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,16 @@ export default function Home() {
     <div box-border>
       <Head>
         <title>自己紹介</title>
+        <meta property='og:url' content='https://fwywd-portfolio-ten.vercel.app/' />
+        <meta property='og:type' content='article' />
+        <meta
+          property='og:image'
+          content='https://fwywd-portfolio-ten.vercel.app/introduction/OGP.jpg'
+        />
+        <meta property='og:title' content='自己紹介' />
+        <meta property='fb:app_id' content='App-ID（15文字の半角数字）' />
+        <meta property='fb:admins' content='adminID（15文字の半角数字）' />
+        <meta name='twitter:card' content='summary_large_image' />
       </Head>
       <Main />
     </div>
