2f2ed1d1cb8c687c26564713d8ffec2136397cfb
niyato2001 <niyato2001@gmail.com>
2022-01-04 10:11:53 +0900

    eslint-plugin-tailwindでのHero修正

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,23 +3,14 @@ import Image from 'next/image';
 
 const Hero = () => (
   <div>
-    <div
-      className='    laptop_l:flex-nowrap laptop_l:items-center
-bg-green-bg w-full h-auto flex-none font-mono flex flex-wrap 
-'
-    >
-      <div
-        className='      laptop_l:ml-32  laptop_l:w-1/2 laptop_l:h-full laptop_l:text-left laptop_l:pt-0
-w-11/12 h-24 ml-auto mr-auto text-center pt-5
-'
-      >
+    <div className='flex flex-wrap flex-none items-center pt-5 font-mono bg-green-bg laptop_l:flex-nowrap laptop_l:pt-0'>
+      <div className='mr-auto ml-auto w-11/12 h-24 text-center laptop_l:ml-32 laptop_l:w-1/2 laptop_l:h-full laptop_l:text-left'>
         <h1 className='text-3xl font-extrabold tracking-widest text-white'>メインタイトル</h1>
         <h2 className='py-2 text-xl font-bold tracking-widest text-green-prolight'>サブタイトル</h2>
       </div>
       <div
-        className='      laptop_l:text-right laptop_l:ml-auto laptop_l:mr-12 laptop_l:py-12 laptop_l:h-full
-w-1/2 h-auto mx-auto text-center pb-5
-'
+        className='      laptop_l:text-right laptop_l:ml-auto laptop_l:mr-12 laptop_l:py-12 
+w-1/2 h-auto mx-auto text-center pb-5'
       >
         <Image src='/introduction/hero-min.png' alt='header' width={560} height={480} />
       </div>
