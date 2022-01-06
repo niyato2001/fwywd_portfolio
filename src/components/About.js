bd47f4aa600acf0471c5cb56948d02c499612bd2
niyato2001 <niyato2001@gmail.com>
2022-01-06 13:53:41 +0900

    定数をpageHeadのように名前変更

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import AboutItems from './AboutItems';
 import Headline from './Headline';
 
 const headName = { japanese: '私について', english: 'ABOUT' };
-const AboutList = [
+const aboutList = [
   { src: '/introduction/about-1.png', alt: 'About1', width: 184, height: 157, name: '趣味' },
   {
     src: '/introduction/about-2.png',
@@ -22,7 +22,7 @@ export default function About() {
         <Headline japanese={headName.japanese} english={headName.english} />
       </div>
       <div className='flex flex-wrap items-end mx-auto w-2/3 h-auto text-center'>
-        {AboutList.map((AboutItem, index) => {
+        {aboutList.map((AboutItem, index) => {
           return (
             <AboutItems
               src={AboutItem.src}
