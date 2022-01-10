4440b083d3e4eddd3e6b7a9bc486dca1dffdfb22
niyato2001 <niyato2001@gmail.com>
2022-01-10 15:34:53 +0900

    PageMainの修正

---
+++ src/components/PageMain.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+import About from './About';
+import Future from './Future';
+import Hero from './Hero';
+import Skills from './Skills';
+import Values from './Values';
+
+export default function PageMain(): JSX.Element {
+  return (
+    <div className='w-full'>
+      <Hero />
+      <About />
+      <Skills />
+      <Values />
+      <Future />
+    </div>
+  );
+}
