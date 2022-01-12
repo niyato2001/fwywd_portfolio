060f0bc25fc165d32945a162c05eec6ba5917bcf
niyato2001 <niyato2001@gmail.com>
2022-01-12 14:06:05 +0900

    ValueItemsコンポーネントを関数化

--- src/components/ValueItems.tsx
+++ src/components/ValueItems.tsx
@@ -7,22 +7,17 @@ interface Props {
   name: string;
 }
 
-export default class ValueItems extends React.Component<Props, {}> {
-  constructor(props: Props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div>
-        <Image src={this.props.src} alt={this.props.alt} width={200} height={200} quality={100} />
-        <p className='py-5 text-lg font-extrabold tracking-wider'>{this.props.name}</p>
-        <div className='items-center text-sm tracking-widest leading-loose text-left text-black-font'>
-          <p className='pb-5 laptop_l:pb-0'>
-            テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
-            テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
-          </p>
-        </div>
+export default function ValueItems(props: Props): JSX.Element {
+  return (
+    <div>
+      <Image src={props.src} alt={props.alt} width={200} height={200} quality={100} />
+      <p className='py-5 text-lg font-extrabold tracking-wider'>{props.name}</p>
+      <div className='items-center text-sm tracking-widest leading-loose text-left text-black-font'>
+        <p className='pb-5 laptop_l:pb-0'>
+          テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+          テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+        </p>
       </div>
-    );
-  }
+    </div>
+  );
 }
