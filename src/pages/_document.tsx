f3cdb8a42f1cebaad8ae8ea58a4c38a79a28c8e3
niyato2001 <niyato2001@gmail.com>
2022-01-12 14:08:47 +0900

    _documentを関数化

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,16 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default class MyDocument extends Document<{}> {
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link rel='icon' type='image /x-icon' sizes='16x16' href='/favicon/fwywd_favicon.ico' />
-        </Head>
-        <body>
-          <Main />
-          <div id='portal'></div>
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function MyDocument(): JSX.Element {
+  return (
+    <Html>
+      <Head>
+        <link rel='icon' type='image /x-icon' sizes='16x16' href='/favicon/fwywd_favicon.ico' />
+      </Head>
+      <body>
+        <Main />
+        <div id='portal'></div>
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
