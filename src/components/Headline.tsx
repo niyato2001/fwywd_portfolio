bb4d526357487f6cf1cacc8520d8bf33a9f98b59
niyato2001 <niyato2001@gmail.com>
2022-01-12 14:02:35 +0900

    Headlineコンポーネントを関数化

--- src/components/Headline.tsx
+++ src/components/Headline.tsx
@@ -5,20 +5,15 @@ interface Props {
   english: string;
 }
 
-export default class Headline extends React.Component<Props, {}> {
-  constructor(props: Props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div>
-        <h1 className='w-full font-mono text-2xl font-bold tracking-widest text-center text-black-font align-middle laptop_l:inline laptop_l:w-1/2'>
-          {this.props.japanese}
-        </h1>
-        <h2 className='w-full font-semibold tracking-wider text-center text-green-font align-middle laptop_l:inline laptop_l:ml-3 laptop_l:w-1/2'>
-          {this.props.english}
-        </h2>
-      </div>
-    );
-  }
+export default function Headline(props: Props): JSX.Element {
+  return (
+    <div>
+      <h1 className='w-full font-mono text-2xl font-bold tracking-widest text-center text-black-font align-middle laptop_l:inline laptop_l:w-1/2'>
+        {props.japanese}
+      </h1>
+      <h2 className='w-full font-semibold tracking-wider text-center text-green-font align-middle laptop_l:inline laptop_l:ml-3 laptop_l:w-1/2'>
+        {props.english}
+      </h2>
+    </div>
+  );
 }
